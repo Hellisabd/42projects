7c0770fe6e9ccdca466d21e63064e7080c911167
Hellisabd <firestone.ow@gmail.com>
2023-10-20 23:04:44 +0200

    atoi proto

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,9 +1,14 @@
+#include "libft.h"
+
 int atoi(const char *nptr)
 {
     size_t  i;
     int     sign;
+    long int     res;
 
     i = 0;
+    sign = 1;
+    res = 0;
     while (nptr[i] != '\0' || (nptr[i] == ' ' && (nptr[i] >= 9 && nptr[i] <= 13)))
         i++;
     if (nptr[i] == '+')
@@ -13,4 +18,12 @@ int atoi(const char *nptr)
             i++;
             sign = -1;
         }
+    while (nptr[i] <= 9 && nptr[i] >= 0)
+    {
+        res = res * 10 + (nptr[i] - 48);
+        i++;
+    }
+    if (res == 2147483648)
+        return (-2147483648);
+    return ((int)res * sign);
 }
