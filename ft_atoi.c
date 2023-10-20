d0b6b0487985579b6566bdd4e77564408e779dd1
Hellisabd <firestone.ow@gmail.com>
2023-10-20 21:49:57 +0200

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -1,10 +1,16 @@
 int atoi(const char *nptr)
 {
     size_t  i;
+    int     sign;
 
     i = 0;
-    while (nptr[i] != '\0' || (nptr[i] ==))
-    {
-
-    }
+    while (nptr[i] != '\0' || (nptr[i] == ' ' && (nptr[i] >= 9 && nptr[i] <= 13)))
+        i++;
+    if (nptr[i] == '+')
+        i++;
+    if (nptr[i] == '-')
+        {
+            i++;
+            sign = -1;
+        }
 }
