450eeef37b4f7d617a30b19732c486fed1d0dd00
Hellisabd <firestone.ow@gmail.com>
2023-10-21 13:58:44 +0200

    memcpy a tester

---
+++ ft_memcpy.c
@@ -0,0 +1,25 @@
+#include "libft.h"
+
+void    *ft_memcpy(void *dest, const void *src, size_t n)
+{
+    size_t  i;
+    char *d;
+    const char *s;
+
+    s = (const char *)src;
+    d = (char *)dest;
+    while (i < n && src[i] != '\0')
+    {
+        d[i] = s[i];
+        i++; 
+    }
+    return (d);
+}
+
+int main(void)
+{
+    const char *src = "bonjour";
+    char *dest[10];
+    printf("ma fonction : %s\n", ft_memcpy(dest, src, 7));
+    printf("ma fonction : %s\n", memcpy(dest, src, 7));
+} 
