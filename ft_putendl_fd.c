4e249bcf890fd9dac92922ba436d844e4bafe450
Basile Grosjean <bgrosjea@k1r2p4.42mulhouse.fr>
2023-10-25 19:10:25 +0200

    all fonctions obligatoires a corriger

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -1,31 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bgrosjea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/17 13:41:59 by bgrosjea          #+#    #+#             */
-/*   Updated: 2023/10/25 19:08:20 by bgrosjea         ###   ########.fr       */
+/*   Created: 2023/10/25 17:56:36 by bgrosjea          #+#    #+#             */
+/*   Updated: 2023/10/25 17:59:48 by bgrosjea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <limits.h>
 
-void    *ft_calloc(size_t nmemb, size_t size)
+void	ft_putendl_fd(char *s, int fd)
 {
-	void	*tab;
-	int	i;
-
-	i = nmemb * size;
-	if (i > INT_MAX)
-		return (NULL);
-	if (nmemb == 0 || size == 0)
-		return (malloc(0));
-	tab = malloc(nmemb * size);
-	if(!tab)
-		return (NULL);
-	ft_bzero(tab, nmemb * size);
-	return (tab);
+	ft_putstr_fd(s, fd);
+	ft_putchar_fd('\n', fd);
 }
